0dfd95302b899f826f534f49dbcfae2fe77ab6a0
Kurtis Denning <kurtisdenning@outlook.com>
2022-08-18 17:51:40 +1200

    dw about it

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
         <>
             <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
                 <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-                    <Box>Logo</Box>
+                    <Box>KD</Box>
                     <HStack spacing={8} alignItems={'center'}>
                         <HStack
                             as={'nav'}
